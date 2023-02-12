700b40bb547303bf5661251fb913bd53b223f2f4
Diman Todorov <diman.todorov@outlook.com>
2023-02-11 21:38:52 -0800

    stuff

---
+++ build/CMakeFiles/dainm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dainm.dir/src/main.cpp.o"
+  "dainm"
+  "dainm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dainm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
